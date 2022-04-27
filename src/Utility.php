06265bc626351d3456fed7c77f0e72943ac260eb
your_github_username <your_github_username>
2022-04-27 11:55:25 +0700

    rp

--- src/Utility.php
+++ src/Utility.php
@@ -7,65 +7,6 @@ use Carbon\Carbon;
 
 class Utility
 {
-    public static function makeViewJson()
-    {
-        if(is_array($tagNames) && count($tagNames) == 1) {
-            $tagNames = reset($tagNames);
-        }
-
-        if(is_string($tagNames)) {
-            $tagNames = explode(',', $tagNames);
-        } elseif(!is_array($tagNames)) {
-            $tagNames = array(null);
-        }
-
-        $tagNames = array_map('trim', $tagNames);
-
-        return array_values($tagNames);
-    }
-
-
-    public static function slug($str)
-    {
-        
-    }
-
-    public static function incrementCount($tagString, $tagSlug, $count)
-    {
-        if($count <= 0) { return; }
-        $model = static::tagModelString();
-
-        $tag = $model::where('slug', '=', $tagSlug)->first();
-
-        if(!$tag) {
-            $tag = new $model;
-            $tag->name = $tagString;
-            $tag->slug = $tagSlug;
-            $tag->suggest = false;
-            $tag->save();
-        }
-
-        $tag->count = $tag->count + $count;
-        $tag->save();
-    }
-
-    public static function decrementCount($tagString, $tagSlug, $count)
-    {
-        if($count <= 0) { return; }
-        $model = static::tagModelString();
-
-        $tag = $model::where('slug', '=', $tagSlug)->first();
-
-        if($tag) {
-            $tag->count = $tag->count - $count;
-            if($tag->count < 0) {
-                $tag->count = 0;
-                \Log::warning("The '.$model.' count for `$tag->name` was a negative number. This probably means your data got corrupted. Please assess your code and report an issue if you find one.");
-            }
-            $tag->save();
-        }
-    }
-
     public static function lookViewByUserModelString()
     {
         return config('look_view.look_view_by_user_model', '\Package\Kennofizet\Lookview\Model\LookViewByUser');
